41879814dfa5752d96fa8ed757a4c7075fde7d58
beni2412 <hardaman97@gmail.com>
2020-09-28 22:20:17 +0530

    [Hardaman] Added: Validation of first name

--- src/com/capg/regex3/UserRegistration.java
+++ src/com/capg/regex3/UserRegistration.java
@@ -1,7 +1,25 @@
 package com.capg.regex3;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserRegistration {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to User Registration");
+		System.out.println("Enter first name (Start with capital)");
+		String fname = sc.nextLine();
+		Pattern pattern =Pattern.compile("^[A-Z]{1}[a-z]{2,}");
+		Matcher matcher = pattern.matcher(fname);
+		boolean matchfound = matcher.find();
+		if(matchfound) {
+			System.out.println("Match found");
+		}
+		else {
+			System.out.println("Incorrect name (Start with capital and should have min 3 charcaters ");
+			
+		
+		}
+		
 	}
 }
