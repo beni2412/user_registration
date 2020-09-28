75103791761b52cb996334c6d65efa0381636a7e
beni2412 <hardaman97@gmail.com>
2020-09-28 22:41:31 +0530

    [Hardaman] Added: Validation of the email ID

--- src/com/capg/regex3/UserRegistration.java
+++ src/com/capg/regex3/UserRegistration.java
@@ -8,7 +8,7 @@ public class UserRegistration {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to User Registration");
 		for(int i=0;;) {
-		System.out.println("Enter first name (Start with capital)");
+		System.out.println("Enter first name (Start with capital):");
 		String fname = sc.nextLine();
 		Pattern pattern =Pattern.compile("^[A-Z]{1}[a-z]{2,}");
 		Matcher matcher = pattern.matcher(fname);
@@ -17,16 +17,32 @@ public class UserRegistration {
 			break;
 		}
 		else {
-			System.out.println("Incorrect name (Start with capital and should have min 3 charcaters ");
+			System.out.println("Invalid name (Start with capital and should have min 3 charcaters ");
 			
 		
 		}
 		}
 		for(int i=0;;) {
-			System.out.println("Enter last name (Start with capital)");
+			System.out.println("Enter last name (Start with capital):");
 			String lname = sc.nextLine();
 			Pattern pattern =Pattern.compile("^[A-Z]{1}[a-z]{2,}");
 			Matcher matcher = pattern.matcher(lname);
+			boolean matchfound = matcher.find();
+			
+			if(matchfound) {
+				break;
+			}
+			else {
+				System.out.println("Invalid name (Start with capital and should have min 3 charcaters ");
+				
+			
+			}
+			}
+		for(int i=0;;) {
+			System.out.println("Enter Email ID: ");
+			String email = sc.nextLine();
+			Pattern pattern =Pattern.compile("^abc([.+_-]{0,1}[0-9a-z]+)?@[a-z0-9]+(\\.[a-z]{2,}){1,2}");
+			Matcher matcher = pattern.matcher(email);
 			boolean matchfound = matcher.find();
 			
 			if(matchfound) {
@@ -34,7 +50,7 @@ public class UserRegistration {
 				break;
 			}
 			else {
-				System.out.println("Incorrect name (Start with capital and should have min 3 charcaters ");
+				System.out.println("Invalid email ");
 				
 			
 			}
